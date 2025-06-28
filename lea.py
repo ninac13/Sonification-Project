baaed784c34eb161b469d4ccf058ad7ec9e9fbb3
Leahwithnoh <leay@nycstudents.net>
2025-06-27 20:26:52 -0400

    Create lea.py

---
+++ lea.py
@@ -0,0 +1,12 @@
+import music21
+print(music21.__version__)
+
+from music21 import note, stream
+
+# Define the DNA-to-note mapping
+nucleotide_to_note = {
+    'A': 'C2',
+    'T': 'G4',
+    'C': 'C6',
+    'G': 'F#7'
+}
