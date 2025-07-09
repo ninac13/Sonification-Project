40649c0bb168dc341c8c2a9d4b6e1d5d01e4c3d2
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-07-09 11:59:46 -0400

    visual1

--- Trial1Views/visualTrial1.py
+++ Trial1Views/visualTrial1.py
@@ -35,7 +35,8 @@ def index():
         markers_used = request.form.get("markers_used")
         mutations_found = request.form.get("mutations_found")
 
-        if time_taken and markers_used and mutations_found is not None:
+        if all([time_taken, markers_used, mutations_found]) and \
+          markers_used.isdigit() and mutations_found.isdigit():
             misplaced = int(markers_used) - int(mutations_found)
             records = {rec.id: str(rec.seq) for rec in SeqIO.parse(SET1_FASTA, "fasta")}
             nonmut = records["trial1_original"]
